942c8d45eb6e3595de0382f3b91efd776cbae94c
Alexandre De Abreu <alde-abr@paul-f3Ar5s5.clusters.42paris.fr>
2024-12-02 16:22:23 +0100

    Adjust : calloc (no longer need bzero)

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: alde-abre <alde-abre@42student.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 11:24:33 by alde-abr          #+#    #+#             */
-/*   Updated: 2024/11/23 10:21:06 by alde-abre        ###   ########.fr       */
+/*   Updated: 2024/12/02 16:20:34 by alde-abre        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,16 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void			*mem;
 	unsigned int	c_size;
+	int				i;
 
+	i = -1;
 	c_size = nmemb * size;
 	if (c_size < nmemb && c_size < size)
 		return (NULL);
 	mem = malloc(c_size);
 	if (!mem)
 		return (NULL);
-	ft_bzero(mem, c_size);
+	while (++i < (int)c_size)
+		*(unsigned char *)(mem + i) = 0;
 	return (mem);
 }
